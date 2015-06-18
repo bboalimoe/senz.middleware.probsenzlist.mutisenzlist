c165dc0f158391a4a0e9a8e12c4a9b41b5d70136
jiaying.lu <lujiaying93@foxmail.com>
2015-06-18 21:55:44 +0800

    change url and token config

--- flask_app/config.py
+++ flask_app/config.py
@@ -5,11 +5,11 @@ import os
 
 # Settings
 
-LOGENTRIES_DEV_TOKEN = "768ebdbc-b889-4a84-9850-f7eecf728aa3"
+LOGENTRIES_TEST_TOKEN = "768ebdbc-b889-4a84-9850-f7eecf728aa3"
 LOGENTRIES_PROD_TOKEN = "27274a9e-8e2f-4e08-ba29-1bbb46c5047d"
 LOGENTRIES_LOCAL_TOKEN = "768ebdbc-b889-4a84-9850-f7eecf728aa3"
 
-BUGSNAG_DEV_TOKEN = "eed4e480fb8fba0597eef2833d45dd4f"
+BUGSNAG_TEST_TOKEN = "eed4e480fb8fba0597eef2833d45dd4f"
 BUGSNAG_PROD_TOKEN = "ccf314cc898d9edeccd67b01b8b79815"
 BUGSNAG_LOCAL_TOKEN = "eed4e480fb8fba0597eef2833d45dd4f"
 
@@ -26,9 +26,9 @@ except KeyError, key:
     print "The local env will be applied"
     APP_ENV = "local"
 finally:
-    if APP_ENV == "dev":
-        LOGENTRIES_TOKEN = LOGENTRIES_DEV_TOKEN
-        BUGSNAG_TOKEN = BUGSNAG_DEV_TOKEN
+    if APP_ENV == "test":
+        LOGENTRIES_TOKEN = LOGENTRIES_TEST_TOKEN
+        BUGSNAG_TOKEN = BUGSNAG_TEST_TOKEN
     elif APP_ENV == "prod":
         LOGENTRIES_TOKEN = LOGENTRIES_PROD_TOKEN
         BUGSNAG_TOKEN = BUGSNAG_PROD_TOKEN
